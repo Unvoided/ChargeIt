aa027b4612a3452fda8aa061b1c5d43b7615a338
DIOGO ALVES MACHADO NETO <8200435@estg.ipp.pt>
2023-02-08 21:20:54 +0000

    Added main activity components

--- app/src/main/java/com/unvoided/chargeit/MainActivity.kt
+++ app/src/main/java/com/unvoided/chargeit/MainActivity.kt
@@ -1,25 +1,50 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.unvoided.chargeit
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.unvoided.chargeit.ui.theme.ChargeItTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ChargeItTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    Scaffold(
+                        topBar = { ChargeItTopBar() },
+                        bottomBar = { ChargeItNavBar(navController) }) { paddingValues ->
+                        ChargeItNavHost(
+                            navController = navController,
+                            paddingValues = paddingValues
+                        )
+                    }
                 }
             }
         }
@@ -27,14 +52,69 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ChargeItTopBar() {
+    TopAppBar(
+        title = { Text("ChargeIt", fontWeight = FontWeight.Bold) },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.08f)
+                .compositeOver(MaterialTheme.colorScheme.surface.copy())
+        ),
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Filled.AccountCircle,
+                    contentDescription = "Account",
+                )
+            }
+        }
+    )
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ChargeItTheme {
-        Greeting("Android")
+fun ChargeItNavHost(navController: NavHostController, paddingValues: PaddingValues) {
+    NavHost(
+        navController = navController,
+        startDestination = "test1",
+        modifier = Modifier.padding(paddingValues)
+    ) {
+        composable("test1") { Text("Hello1") }
+        composable("test2") { Text("Hello2") }
+        composable("test3") { Text("Hello3") }
     }
 }
+
+@Composable
+fun ChargeItNavBar(navController: NavController) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+    val chargeItNavigationItems = listOf("test1", "test2", "test3")
+
+    NavigationBar {
+        chargeItNavigationItems.forEachIndexed { _, item ->
+            val selected = currentDestination?.hierarchy?.any { it.route == item } == true
+
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                label = {
+                    Text(
+                        text = item,
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                },
+                selected = selected,
+                onClick = {
+                    navController.navigate(item) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+            )
+
+        }
+    }
+}
+
--- app/src/main/java/com/unvoided/chargeit/ui/theme/Theme.kt
+++ app/src/main/java/com/unvoided/chargeit/ui/theme/Theme.kt
@@ -3,19 +3,13 @@ package com.unvoided.chargeit.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.compositeOver
-import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
@@ -58,11 +52,19 @@ fun ChargeItTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            val activity  = view.context as Activity
-            activity.window.navigationBarColor = colorScheme.background.toArgb()
-            activity.window.statusBarColor = colorScheme.background.toArgb()
-            WindowCompat.getInsetsController(activity.window, view).isAppearanceLightStatusBars = !darkTheme
-            WindowCompat.getInsetsController(activity.window, view).isAppearanceLightNavigationBars = !darkTheme
+            val activity = view.context as Activity
+            activity.window.navigationBarColor =
+                colorScheme.primary.copy(alpha = 0.08f).compositeOver(colorScheme.surface.copy())
+                    .toArgb()
+            activity.window.statusBarColor =
+                colorScheme.primary.copy(alpha = 0.08f).compositeOver(colorScheme.surface.copy())
+                    .toArgb()
+            WindowCompat.getInsetsController(activity.window, view).isAppearanceLightStatusBars =
+                !darkTheme
+            WindowCompat.getInsetsController(
+                activity.window,
+                view
+            ).isAppearanceLightNavigationBars = !darkTheme
         }
     }
 
