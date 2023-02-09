90c24b42467de51e2b191d17760695919e9a5b27
DIOGO ALVES MACHADO NETO <8200435@estg.ipp.pt>
2023-02-09 14:45:29 +0000

    Added pages

--- app/src/main/java/com/unvoided/chargeit/MainActivity.kt
+++ app/src/main/java/com/unvoided/chargeit/MainActivity.kt
@@ -11,10 +11,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.Map
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -24,6 +27,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.unvoided.chargeit.pages.ChargersMap
+import com.unvoided.chargeit.pages.Favorites
+import com.unvoided.chargeit.pages.History
 import com.unvoided.chargeit.ui.theme.ChargeItTheme
 
 class MainActivity : ComponentActivity() {
@@ -75,12 +81,12 @@ fun ChargeItTopBar() {
 fun ChargeItNavHost(navController: NavHostController, paddingValues: PaddingValues) {
     NavHost(
         navController = navController,
-        startDestination = "test1",
+        startDestination = Pages.ChargersMapPage.route,
         modifier = Modifier.padding(paddingValues)
     ) {
-        composable("test1") { Text("Hello1") }
-        composable("test2") { Text("Hello2") }
-        composable("test3") { Text("Hello3") }
+        composable(Pages.ChargersMapPage.route) { ChargersMap() }
+        composable(Pages.HistoryPage.route) { History() }
+        composable(Pages.FavoritesPage.route) { Favorites() }
     }
 }
 
@@ -88,23 +94,22 @@ fun ChargeItNavHost(navController: NavHostController, paddingValues: PaddingValu
 fun ChargeItNavBar(navController: NavController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
-    val chargeItNavigationItems = listOf("test1", "test2", "test3")
 
     NavigationBar {
-        chargeItNavigationItems.forEachIndexed { _, item ->
-            val selected = currentDestination?.hierarchy?.any { it.route == item } == true
+        Pages.values().forEach { item ->
+            val selected = currentDestination?.hierarchy?.any { it.route == item.route } == true
 
             NavigationBarItem(
-                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                icon = { Icon(item.icon, contentDescription = item.label) },
                 label = {
                     Text(
-                        text = item,
-                        fontWeight = FontWeight.ExtraBold
+                        text = item.label,
+                        fontWeight = FontWeight.Bold
                     )
                 },
                 selected = selected,
                 onClick = {
-                    navController.navigate(item) {
+                    navController.navigate(item.route) {
                         popUpTo(navController.graph.findStartDestination().id) {
                             saveState = true
                         }
@@ -115,6 +120,12 @@ fun ChargeItNavBar(navController: NavController) {
             )
 
         }
+
     }
 }
 
+enum class Pages(val route: String, val label: String, val icon: ImageVector) {
+    HistoryPage("history", "History", Icons.Filled.History),
+    ChargersMapPage("map", "Map", Icons.Filled.Map),
+    FavoritesPage("favorites", "Favorites", Icons.Filled.Favorite)
+}
---
+++ app/src/main/java/com/unvoided/chargeit/pages/Favorites.kt
@@ -0,0 +1,9 @@
+package com.unvoided.chargeit.pages
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Favorites() {
+    Text("Favorites")
+}
